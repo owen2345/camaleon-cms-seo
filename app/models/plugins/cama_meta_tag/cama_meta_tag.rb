79e3eb483800f4eeee9f7984fe6dd94b7088fd1c
Brian Kephart <briantkephart@gmail.com>
2019-09-08 14:45:34 -0500

    Zeitwerk compatibility

--- app/models/plugins/cama_meta_tag/cama_meta_tag.rb
+++
@@ -1,6 +0,0 @@
-# class Plugins::CamaMetaTag::CamaMetaTag < ActiveRecord::Base
-  # belongs_to :site, class_name: "CamleonCms::Site"
-
-  # here create your models normally
-  # notice: your tables in database will be plugins_cama_meta_tag in plural (check rails documentation)
-# end
